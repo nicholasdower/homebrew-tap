ec8c0799ba9678eb201e0c49f0dde0af0739ef53
Nick Dower <nicholasdower@gmail.com>
2024-02-12 11:05:27 +0100

    Fix joyn file location.

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 curl --silent 'https://raw.githubusercontent.com/nicholasdower/git-coauthor/master/Formula/git-coauthor.rb' > git-coauthor.rb
-curl --silent 'https://raw.githubusercontent.com/nicholasdower/joyn/master/Formula/joyn.rb' > git-coauthor.rb
+curl --silent 'https://raw.githubusercontent.com/nicholasdower/joyn/master/Formula/joyn.rb' > joyn.rb
 
 git config user.email "nicholasdower@gmail.com"
 git config user.name "homebrew-tap-ci"
+
+git add .
 git commit -a -m 'Update formulas.'
 git push
