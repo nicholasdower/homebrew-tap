1d33e3c23f7986596743895c5a222741276a93de
homebrew-tap-ci <nicholasdower@gmail.com>
2024-02-14 09:31:38 +0000

    Update formulas.

--- quote.rb
+++ quote.rb
@@ -1,9 +1,15 @@
 class Quote < Formula
   desc "Quote lines"
   homepage "https://github.com/nicholasdower/quote"
-  url "https://github.com/nicholasdower/quote/releases/download/v1.0.0/release.tar.gz"
-  sha256 "57d5ea39c8138dbe2d12c7fdf3947d680750b180aa6d677bc925c5c7ede95aba"
   license "MIT"
+  version "1.1.0"
+  if Hardware::CPU.arm?
+    url "https://github.com/nicholasdower/quote/releases/download/v1.1.0/quote-1.1.0-aarch64-apple-darwin.tar.gz"
+    sha256 "5b5b97b1293a3af2e10cdf85014e902e7cbd913180f75f9465653550c1f1296f"
+  elsif Hardware::CPU.intel?
+    url "https://github.com/nicholasdower/quote/releases/download/v1.1.0/quote-1.1.0-x86_64-apple-darwin.tar.gz"
+    sha256 "db36872a02452c01ad23efad42ea61c4363a765fcf6b2edd79a76b3f58375bb0"
+  end
 
   def install
     bin.install "bin/quote"
