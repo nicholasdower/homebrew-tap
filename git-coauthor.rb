bd1ac25ff2dadc58364986cfd2e6790099fdd12c
homebrew-tap-ci <nicholasdower@gmail.com>
2024-02-16 18:09:00 +0000

    Update formulas.

--- git-coauthor.rb
+++ git-coauthor.rb
@@ -2,17 +2,26 @@ class GitCoauthor < Formula
   desc "List or add Git coauthors"
   homepage "https://github.com/nicholasdower/git-coauthor"
   license "MIT"
-  version "5.2.0"
-  if Hardware::CPU.arm?
-    url "https://github.com/nicholasdower/git-coauthor/releases/download/v5.2.0/git-coauthor-5.2.0-aarch64-apple-darwin.tar.gz"
-    sha256 "05606298cdfe26f16cc52b51ab6a7fceade4f363faf77ab68656791d49c181b8"
-  elsif Hardware::CPU.intel?
-    url "https://github.com/nicholasdower/git-coauthor/releases/download/v5.2.0/git-coauthor-5.2.0-x86_64-apple-darwin.tar.gz"
-    sha256 "d9290d9b961ae394118555adf013e80f055199f5855847e0fa40d5d294eb34cd"
+  version "5.2.1"
+
+  url "https://github.com/nicholasdower/git-coauthor/releases/download/v5.2.1/git-coauthor-5.2.1.tar.gz"
+  sha256 "186d5719267bde3c64b93dd5e7f4833074918d2669a764e0e1279aee3663df52"
+
+  bottle do
+    rebuild 1
+    root_url "https://github.com/nicholasdower/git-coauthor/releases/download/v5.2.1/"
+    sha256 cellar: :any, monterey: "39965896d182fa2b1ed47d32043b8012ce1dfcb9453d198c1cf6dab94c8d0610"
+    sha256 cellar: :any, ventura: "39965896d182fa2b1ed47d32043b8012ce1dfcb9453d198c1cf6dab94c8d0610"
+    sha256 cellar: :any, sonoma: "39965896d182fa2b1ed47d32043b8012ce1dfcb9453d198c1cf6dab94c8d0610"
+    sha256 cellar: :any, arm64_sonoma: "1b6af0dd3a6cf202c7914fb8c7a35aebd8844482293d333bbe22170e6cb309cf"
+    sha256 cellar: :any, arm64_monterey: "1b6af0dd3a6cf202c7914fb8c7a35aebd8844482293d333bbe22170e6cb309cf"
+    sha256 cellar: :any, arm64_ventura: "1b6af0dd3a6cf202c7914fb8c7a35aebd8844482293d333bbe22170e6cb309cf"
   end
 
+  depends_on "rust" => :build
+
   def install
-    bin.install "bin/git-coauthor"
+    system "cargo", "install", *std_cargo_args
     man1.install "man/git-coauthor.1"
   end
 
