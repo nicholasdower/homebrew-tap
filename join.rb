07d96844643ec2f1d743cce7f308a1ebc8f8edea
Nick Dower <nicholasdower@gmail.com>
2024-02-23 10:54:24 +0100

    Add join caveats.

--- join.rb
+++ join.rb
@@ -25,6 +25,17 @@ class Join < Formula
     man1.install "man/join.1"
   end
 
+  def caveats
+    puts <<~MSG
+      join has been installed but may not be first on your $PATH.
+
+      Consider adding the following to your bashrc:
+
+          export PATH="#{prefix}/bin:$PATH"
+
+    MSG
+  end
+
   test do
     assert_match "join", shell_output("#{bin}/join --version")
   end
