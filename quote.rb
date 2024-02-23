76c8378e490794e6985e23b960ce400c6868e4e0
homebrew-tap-ci <nicholasdower@gmail.com>
2024-02-23 12:23:34 +0000

    Update formulas.

--- quote.rb
+++ quote.rb
@@ -2,20 +2,20 @@ class Quote < Formula
   desc "Quote lines"
   homepage "https://github.com/nicholasdower/quote"
   license "MIT"
-  version "2.0.0"
+  version "3.0.0"
 
-  url "https://github.com/nicholasdower/quote/releases/download/v2.0.0/quote-2.0.0.tar.gz"
-  sha256 "7e1747f3566e1d417925ee318acd767463c3980d429f48ca3e548a04d168bfbe"
+  url "https://github.com/nicholasdower/quote/releases/download/v3.0.0/quote-3.0.0.tar.gz"
+  sha256 "e566dd2c0c6c953ae60359f01453dae69690baf3c7ef671e92445db9f2a8baf7"
 
   bottle do
     rebuild 1
-    root_url "https://github.com/nicholasdower/quote/releases/download/v2.0.0/"
-    sha256 cellar: :any, monterey: "a4570d709a301865a825b57627261e4ca978b6e225ad2f2c5fd143f6a5d6240a"
-    sha256 cellar: :any, ventura: "a4570d709a301865a825b57627261e4ca978b6e225ad2f2c5fd143f6a5d6240a"
-    sha256 cellar: :any, sonoma: "a4570d709a301865a825b57627261e4ca978b6e225ad2f2c5fd143f6a5d6240a"
-    sha256 cellar: :any, arm64_sonoma: "b0b336613efd054ddd70769065071ef8414f3ce81b2bc4375e878dcebf9874c0"
-    sha256 cellar: :any, arm64_monterey: "b0b336613efd054ddd70769065071ef8414f3ce81b2bc4375e878dcebf9874c0"
-    sha256 cellar: :any, arm64_ventura: "b0b336613efd054ddd70769065071ef8414f3ce81b2bc4375e878dcebf9874c0"
+    root_url "https://github.com/nicholasdower/quote/releases/download/v3.0.0/"
+    sha256 cellar: :any, monterey: "05837bf5dcebfc444eb3a42a89ad52b2470c4f5e17ee01a2bb8df70b8c615836"
+    sha256 cellar: :any, ventura: "05837bf5dcebfc444eb3a42a89ad52b2470c4f5e17ee01a2bb8df70b8c615836"
+    sha256 cellar: :any, sonoma: "05837bf5dcebfc444eb3a42a89ad52b2470c4f5e17ee01a2bb8df70b8c615836"
+    sha256 cellar: :any, arm64_sonoma: "899e83122d63f7c79d93cfb30ecbb943dc6b8acba0b6bc51fb663b399cb90560"
+    sha256 cellar: :any, arm64_monterey: "899e83122d63f7c79d93cfb30ecbb943dc6b8acba0b6bc51fb663b399cb90560"
+    sha256 cellar: :any, arm64_ventura: "899e83122d63f7c79d93cfb30ecbb943dc6b8acba0b6bc51fb663b399cb90560"
   end
 
   depends_on "rust" => :build
