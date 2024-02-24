833e3c08857de9779ede32e0a562927b98d5d065
Nick Dower <nicholasdower@gmail.com>
2024-02-24 10:09:56 +0100

    Add awake.

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 curl --silent 'https://raw.githubusercontent.com/nicholasdower/git-coauthor/master/Formula/git-coauthor.rb' > git-coauthor.rb
 curl --silent 'https://raw.githubusercontent.com/nicholasdower/join/master/Formula/join.rb' > join.rb
 curl --silent 'https://raw.githubusercontent.com/nicholasdower/quote/master/Formula/quote.rb' > quote.rb
+curl --silent 'https://raw.githubusercontent.com/nicholasdower/awake/master/Formula/awake.rb' > awake.rb
 
 git config user.email "nicholasdower@gmail.com"
 git config user.name "homebrew-tap-ci"
