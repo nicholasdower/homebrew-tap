a892c489d556b375fdfaea5c0b1eb1fb93339d59
homebrew-tap-ci <nicholasdower@gmail.com>
2024-02-24 13:15:25 +0000

    Update formulas.

--- awake.rb
+++ awake.rb
@@ -2,20 +2,20 @@ class Awake < Formula
   desc "Keep your Mac awake"
   homepage "https://github.com/nicholasdower/awake"
   license "MIT"
-  version "1.1.1"
+  version "1.1.2"
 
-  url "https://github.com/nicholasdower/awake/releases/download/v1.1.1/awake-1.1.1.tar.gz"
-  sha256 "08c2d49ca258d591b985a00b64b18eea5377e9ab7c0face13d42080f9a3cd85d"
+  url "https://github.com/nicholasdower/awake/releases/download/v1.1.2/awake-1.1.2.tar.gz"
+  sha256 "9bdacd323e7a2f883e699e95d51d7828a3d8b10a34eb4b6737c1170cfa11862f"
 
   bottle do
     rebuild 1
-    root_url "https://github.com/nicholasdower/awake/releases/download/v1.1.1/"
-    sha256 cellar: :any, monterey: "2925cf8e500833a0dabebb6392adcfdaf06c5ba3aea4b8d8a37eb10ee8c05e11"
-    sha256 cellar: :any, ventura: "2925cf8e500833a0dabebb6392adcfdaf06c5ba3aea4b8d8a37eb10ee8c05e11"
-    sha256 cellar: :any, sonoma: "2925cf8e500833a0dabebb6392adcfdaf06c5ba3aea4b8d8a37eb10ee8c05e11"
-    sha256 cellar: :any, arm64_sonoma: "9e0b2db97a09ead81f3106762f5b9ab4fd3314d9b3ad03d54531b0aa15a1d50b"
-    sha256 cellar: :any, arm64_monterey: "9e0b2db97a09ead81f3106762f5b9ab4fd3314d9b3ad03d54531b0aa15a1d50b"
-    sha256 cellar: :any, arm64_ventura: "9e0b2db97a09ead81f3106762f5b9ab4fd3314d9b3ad03d54531b0aa15a1d50b"
+    root_url "https://github.com/nicholasdower/awake/releases/download/v1.1.2/"
+    sha256 cellar: :any, monterey: "2e986d3f7b02a5c89822c122ac3fd7aa50e6a205373109159fc101817a7a37a0"
+    sha256 cellar: :any, ventura: "2e986d3f7b02a5c89822c122ac3fd7aa50e6a205373109159fc101817a7a37a0"
+    sha256 cellar: :any, sonoma: "2e986d3f7b02a5c89822c122ac3fd7aa50e6a205373109159fc101817a7a37a0"
+    sha256 cellar: :any, arm64_sonoma: "f7191d0c1793560a535e569dad661ef1dd0edc2724d3ac1b73ee376cf5d22742"
+    sha256 cellar: :any, arm64_monterey: "f7191d0c1793560a535e569dad661ef1dd0edc2724d3ac1b73ee376cf5d22742"
+    sha256 cellar: :any, arm64_ventura: "f7191d0c1793560a535e569dad661ef1dd0edc2724d3ac1b73ee376cf5d22742"
   end
 
   depends_on "rust" => :build
